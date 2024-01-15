fe14804353bde7fb59bffe597acd360ce92692eb
skyemperor611 <blackmonarch124@gmail.com>
2024-01-16 02:53:07 +0900

    archiving 까지 작성

--- src/components/sections/Archiving.js
+++ src/components/sections/Archiving.js
@@ -1,11 +1,27 @@
 import React from "react"
 
 const Archiving = () => {
+  const handleCardClick = (url) => {
+    window.location.href = url
+  }
+
   return (
     <section id="archiving" className="section-class">
-      {/* 섹션 컨텐츠 */}
-      <h2>Archiving</h2>
-      {/* 기타 내용 */}
+      <div className="py-12 text-white bg-black">
+        <div className="mb-10 text-4xl text-center">ARCHIVING</div>
+        <div className="flex justify-center gap-10">
+          <div
+            className="h-64 p-4 text-black transition duration-500 transform bg-white cursor-pointer w-80 hover:translate-y-2"
+            onClick={() => handleCardClick("https://github.com/skyemperor611")}
+          >
+            <h3 className="text-2xl font-bold">GitHub</h3>
+            <p>https://github.com/skyemperor611</p>
+            <ul>
+              <li>개인 프로젝트 연습 코드</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,11 +1,69 @@
 import React from "react"
+import htmlIcon from "../../assets/images/skills/HTML5_Badge_512.png"
+import reactIcon from "../../assets/images/skills/React_Logo.png"
+import springIcon from "../../assets/images/skills/Spring_Logo.png"
+import mysql from "../../assets/images/skills/MySql_Logo.png"
 
 const Skills = () => {
+  const frontendSkills = [
+    { name: "HTML", icon: htmlIcon },
+    { name: "React", icon: reactIcon },
+  ]
+  const backendSkills = [{ name: "SPRING", icon: springIcon }]
+  const dbSkills = [{ name: "mysql", icon: mysql }]
+
   return (
-    <section id="skills" className="section-class">
-      {/* 섹션 컨텐츠 */}
-      <h2>Skills</h2>
-      {/* 기타 내용 */}
+    <section id="skills" className="p-10 bg-yellow-300">
+      <div className="container mx-auto">
+        <h2 className="mb-6 text-4xl font-bold text-center">Skills</h2>
+        <div className="flex flex-wrap justify-center gap-20">
+          <div className="w-full mb-8 md:w-auto">
+            <h3 className="mb-4 text-3xl font-bold text-center">Frontend</h3>
+            <div className="flex justify-center gap-4">
+              {frontendSkills.map((skill) => (
+                <div key={skill.name} className="flex flex-col items-center">
+                  <img
+                    src={skill.icon}
+                    alt={skill.name}
+                    className="w-16 h-16"
+                  />
+                  <span className="mt-2">{skill.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="w-full mb-8 md:w-auto">
+            <h3 className="mb-4 text-3xl font-bold text-center">Backend</h3>
+            <div className="flex justify-center gap-4">
+              {backendSkills.map((skill) => (
+                <div key={skill.name} className="flex flex-col items-center">
+                  <img
+                    src={skill.icon}
+                    alt={skill.name}
+                    className="w-16 h-16"
+                  />
+                  <span className="mt-2">{skill.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="w-full mb-8 md:w-auto">
+            <h3 className="mb-4 text-3xl font-bold text-center">Database</h3>
+            <div className="flex justify-center gap-4">
+              {dbSkills.map((skill) => (
+                <div key={skill.name} className="flex flex-col items-center">
+                  <img
+                    src={skill.icon}
+                    alt={skill.name}
+                    className="w-16 h-16"
+                  />
+                  <span className="mt-2">{skill.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
