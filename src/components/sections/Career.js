dbce495c4eb3027607c9a41e89c59b9b67e5a6a9
skyemperor611 <blackmonarch124@gmail.com>
2024-01-18 03:17:40 +0900

    career 부분 작성 중

--- src/components/sections/Career.js
+++ src/components/sections/Career.js
@@ -1,13 +1,58 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 const Career = () => {
+  const cardContents = [
+    { title: "Project1", description: "project1 description" },
+    {
+      title: "Project2",
+      description: "project2 description",
+    },
+  ]
+
+  const [visibleCards, setVisibleCards] = useState([])
+  const cardRefs = useRef([])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const index = cardRefs.current.indexOf(entry.target)
+          if (entry.isIntersecting) {
+            setVisibleCards((prevVisibleCards) => [...prevVisibleCards, index])
+          }
+        })
+      },
+      { threshold: 0.1 }
+    )
+
+    cardRefs.current.forEach((ref) => ref && observer.observe(ref))
+
+    return () => {
+      cardRefs.current.forEach((ref) => ref && observer.unobserve(ref))
+    }
+  }, [])
+
+  const createCard = (content, index) => (
+    <div
+      ref={(el) => (cardRefs.current[index] = el)}
+      key={index}
+      className={`transition-opacity duration-500 ${
+        visibleCards.includes(index) ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <h3>{content.title}</h3>
+      <p>{content.description}</p>
+    </div>
+  )
+
   return (
-    <section id="career" className="section-class">
-      {/* 섹션 컨텐츠 */}
-      <h2>Career</h2>
-      {/* 기타 내용 */}
+    <section id="career" className="py-12 text-white bg-orange-500">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {cardContents.map((content, index) => createCard(content, index))}
+        </div>
+      </div>
     </section>
   )
 }
-
 export default Career
